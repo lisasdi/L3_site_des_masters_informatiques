ce3f7d6c18506ddd72cc07c765d0c3392a15d45e
lisasdi <79017844+lisasdi@users.noreply.github.com>
2024-10-30 19:12:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,157 +1,166 @@
 import pandas as pd
+import numpy as np
 import configparser
-from geocodage import load_insee_data
 
 # Configuration
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+# Traiter la colonne genre pour gérer les genres multiples
+def choose_random_gender(gender_string):
+    # Sépare les genres par la virgule et choisit un genre aléatoire
+    genders = [g.strip().upper() for g in gender_string.split(',')]
+    return np.random.choice(genders)
+# Fonction enrichissement_age mise à jour avec fusion de la table des prénoms
+def EG_age_sexe(tb_client, prenom, sexe="NA", age_declare="NA", top_estim_sexe=1, codgeo="codgeo", ajust=0, var_ajust="NA"):
+
+    fichier_prenom_sexe = pd.read_csv(config['paths']['prenoms'],sep=';', usecols=["01_prenom", "02_genre"],encoding='latin1')
+
+    # Renommer les colonnes pour simplifier le mapping
+    fichier_prenom_sexe.columns = ["prenom", "genre"]
+    fichier_prenom_sexe["prenom"]=fichier_prenom_sexe["prenom"].str.lower()
 
 
-def EG_Enrichissement_Geomk(table_initiale, codgeo, var_sexe, var_age):
-    
-    # Vérification que les colonnes nécessaires existent dans la table_initiale
-    required_columns = [codgeo, var_sexe, var_age]
-    for col in required_columns:
-        if col not in table_initiale.columns:
-            raise ValueError(f"La colonne {col} est absente de la table_initiale.")
-    
-    # Création d'une copie de la table pour enrichir les données
-    enriched_data = table_initiale.copy()
-    
-    # Ajout des informations socio-démographiques simulées (exemple fictif)
-    column_names = ['rev','t_comm','typocom','locat_hlm','c_indice_qualite_rev',
-                    'c_indice_qualite_pcs','h_ind','c_indice_qualite_formation',
-                    'c_indice_qualite_logement','c_indice_qualite_menage','codgeo']  
-    maj = pd.read_csv(config['paths']['majdata'], sep=',', names=column_names,low_memory=False)
 
-    # Fusion des informations régionales à partir du reg
-    enriched_data = enriched_data.merge(
-        maj,
-        left_on='codgeo',
-        right_on='codgeo',  
-        how='left'
+
+    # Appliquer la fonction pour choisir un genre
+    fichier_prenom_sexe["genre"] = fichier_prenom_sexe["genre"].apply(choose_random_gender)
+    tb_client["prenom"]=tb_client["prenom"].str.lower()
+    # Estimation du sexe en fonction de top_estim_sexe
+    if top_estim_sexe == 1:
+        # Fusion des tables pour obtenir le sexe estimé
+        tb_client = tb_client.merge(fichier_prenom_sexe, how='left', left_on=prenom, right_on="prenom")
+        # Créer e_sexe en fonction de la présence de la colonne sexe
+        if sexe=="NA"  or sexe not in tb_client.columns:
+            tb_client["e_sexe"] = tb_client["genre"].fillna("Inconnu")  # Remplir avec le genre estimé
+            
+        else: 
+            tb_client["e_sexe"] = np.where(tb_client[sexe].isna(),tb_client["genre"].fillna("Inconnu"),tb_client[sexe]) # Si prénom absent dans le fichier, marquer comme "Inconnu"
+    
+    # Suppression de la colonne "genre" (pour éviter les doublons)
+    tb_client.drop(columns=["genre"], inplace=True)
+
+    annee=int(config['paths']['annee'])
+
+    # Estimation de l'année de naissance en utilisant `age_declare` si disponible
+    if age_declare!="NA"  and age_declare in tb_client.columns:
+        tb_client["e_annee_naissance"] = np.where(
+            tb_client[age_declare].notna(),
+            annee - tb_client[age_declare],
+            annee - np.random.randint(1, 100, tb_client.shape[0])
+        )
+    else: 
+        tb_client["e_annee_naissance"] = annee - np.random.randint(1, 100, tb_client.shape[0])
+    # Ajout d'autres champs simulés pour l'exemple
+    tb_client["e_age"] = annee - tb_client["e_annee_naissance"]
+    # Ajustement de l'âge estimé si `ajust` est activé
+    if ajust == 1:
+        if var_ajust == "sexe" and sexe!="NA" :
+            mean_age_by_sex = tb_client.groupby(sexe)["e_age"].transform("mean")
+            tb_client["e_age"] = tb_client["e_age"] * 0.5 + mean_age_by_sex * 0.5
+        else:
+            mean_age = tb_client["e_age"].mean()
+            tb_client["e_age"] = tb_client["e_age"] * 0.5 + mean_age * 0.5
+    tb_client["e_age"] = tb_client["e_age"].round().astype(int)
+
+    tb_client["age_reel"] =np.random.randint(0, 100, tb_client.shape[0])  # Simuler un âge réel entre 0 et 99 ans
+
+    # Définir la plage pour l'âge déclaré s'il est disponible
+    if age_declare!="NA"  and age_declare in tb_client.columns:
+        tb_client["age_plage_min"] = np.where(
+            age_declare!="NA",
+            tb_client[age_declare].astype(float) - 5,
+            tb_client["e_age"] - 5
+        )
+        tb_client["age_plage_max"] = np.where(
+            age_declare !="NA",
+            tb_client[age_declare].astype(float) + 5,
+            tb_client["e_age"] + 5
+        )
+    else : 
+        tb_client["age_plage_min"] =tb_client["e_age"] - 5
+        tb_client["age_plage_max"] =tb_client["e_age"] + 5
+    tb_client["difference"]=(tb_client["age_reel"]-tb_client["e_age"]).abs()
+
+    # Vérifier si age_reel est dans la plage
+    tb_client["e_p_5ans"] = np.where(
+        (tb_client["age_reel"] >= tb_client["age_plage_min"]) & (tb_client["age_reel"] <= tb_client["age_plage_max"]),
+        1.0- (tb_client["difference"]/5),  # Probabilité = 1 (dans la fourchette)
+        0.0   # Probabilité = 0 (en dehors de la fourchette)
     )
-    
-    # Renommage des colonnes pour correspondre aux champs de sortie
-    enriched_data.rename(columns={  
-        'rev': 'i_rev',
-        't_comm': 'e_taille_commune',
-        'typocom': 'e_typo_commune_2010'         
 
-    }, inplace=True)
+    # Vérification si l'âge estimé est aussi dans la plage de +/- 5 de l'âge déclaré
+    if age_declare!="NA"  and age_declare in tb_client.columns:
+        tb_client["indice_conf_age"] = np.where(
+            tb_client["age_declare"].notna(),  # Vérifie si age_declare est présent
+            np.where(
+            (tb_client["e_age"] >= tb_client["age_declare"] - 5) & (tb_client["e_age"] <= tb_client["age_declare"] + 5),
+            "Confiance ++",  # Âge estimé dans l'intervalle de l'âge déclaré
+            np.where(
+            (tb_client["e_age"] >= tb_client["age_declare"] - 3) & (tb_client["e_age"] <= tb_client["age_declare"] + 3),
+            "Confiance +",  # Âge estimé proche de l'âge déclaré
+            "Confiance -"   # Âge estimé éloigné de l'âge déclaré
+            )
+            ),
+            # Si age_declare n'est pas présent, on compare avec age_reel
+            np.where(
+                (tb_client["e_age"] >= tb_client["age_reel"] - 5) & (tb_client["e_age"] <= tb_client["age_reel"] + 5),
+                "Confiance ++",  # Âge estimé dans l'intervalle de l'âge réel
+                np.where(
+                    (tb_client["e_age"] >= tb_client["age_reel"] - 3) & (tb_client["e_age"] <= tb_client["age_reel"] + 3),
+                    "Confiance +",  # Âge estimé proche de l'âge réel
+                    "Confiance -"   # Âge estimé éloigné de l'âge réel
+                )
+            )
+        )
+    else:
+        tb_client["indice_conf_age"]=np.where(
+                (tb_client["e_age"] >= tb_client["age_reel"] - 5) & (tb_client["e_age"] <= tb_client["age_reel"] + 5),
+                "Confiance ++",  # Âge estimé dans l'intervalle de l'âge réel
+                np.where(
+                    (tb_client["e_age"] >= tb_client["age_reel"] - 3) & (tb_client["e_age"] <= tb_client["age_reel"] + 3),
+                    "Confiance +",  # Âge estimé proche de l'âge réel
+                    "Confiance -"   # Âge estimé éloigné de l'âge réel
+                )
+            )
 
-    
-    # Lecture du fichier CSV contenant les probabilités d'enfants
-    sexe_age_proba = pd.read_csv(config['paths']['enfants'])
 
 
-    # Création de la colonne `sexe_age` dans `enriched_data` et `sexe_age_proba` pour la fusion
-    enriched_data["sexe_age"] = enriched_data[var_sexe] + enriched_data[var_age].astype(str)
+    # Suppression de la colonne 
+    tb_client.drop(columns=["age_plage_min"], inplace=True)
+    tb_client.drop(columns=["difference"], inplace=True)
+    tb_client.drop(columns=["age_plage_max"], inplace=True)
+    tb_client.drop(columns=["age_reel"], inplace=True)
 
-    
-    # Fusion des tables en utilisant `sexe_age` comme clé
-    enriched_data = enriched_data.merge(sexe_age_proba, on="sexe_age", how="left")
-    
-    # Renommer les colonnes importées depuis le CSV pour qu'elles correspondent aux champs de sortie
-    enriched_data.rename(columns={
-        "p_1enf": "e_proba_1_enfant",
-        "p_2enf": "e_proba_2_enfants",
-        "p_m5": "e_proba_m5",
-        "p_5_10": "e_proba_5_10",
-        "p_10_15": "e_proba_10_15",
-        "p_15_20": "e_proba_15_20"
-    }, inplace=True)
-    
-    # Suppression de la colonne temporaire `sexe_age`
-    enriched_data.drop(columns="sexe_age", inplace=True)
-    
-    # Informations géographiques et autres enrichissements fictifs pour compléter
-    '''  
-    enriched_data['e_seg_logement'] = "Logement A"
-    enriched_data['e_revenus'] = "Moyen"
-    enriched_data['e_decile'] = 5
-    enriched_data['e_PCS'] = "Catégorie X"
-    enriched_data['e_situation_fam'] = "Célibataire"
-    enriched_data['e_etudes'] = "Licence
 
-    '''
-    # Lecture du fichier CSV pour informations régionales
-    region_data=load_insee_data()
-    region_data=region_data[['Code INSEE','Département','Région','Code Département','Code Région']]
-    
-    # Extraction du code INSEE depuis le code géographique (5 premiers caractères)
-    enriched_data['Code INSEE'] = enriched_data[codgeo].str[:5]
-    
-    # Fusion des informations régionales à partir du Code INSEE
-    enriched_data = enriched_data.merge(
-        region_data,
-        left_on='Code INSEE',
-        right_on='Code INSEE',  # Assurez-vous que cette colonne est bien nommée "Département" dans le fichier
-        how='left'
-    )
-    
-    # Renommage des colonnes pour correspondre aux champs de sortie
-    enriched_data.rename(columns={
-        'Département': 'e_lib_dept',     # Département de résidence
-        'Région': 'e_Region',        # Région de résidence
-        'Code Région': 'e_reg' ,
-        'Code Département': 'e_dept'      # Numéro de région de résidence
-    }, inplace=True)
-    enriched_data.drop(columns=["Code INSEE"], inplace=True)
-    # Nettoyer la colonne pour retirer les crochets et les apostrophes
-    enriched_data['e_Region'] = enriched_data['e_Region'].str.strip("[]").str.replace("'", "", regex=False)
-    enriched_data['e_dept'] = enriched_data['e_dept'].str.strip("[]").str.replace("'", "", regex=False)
-    enriched_data['e_lib_dept'] = enriched_data['e_lib_dept'].str.strip("[]").str.replace("'", "", regex=False)
-    enriched_data['e_reg'] = enriched_data['e_reg'].fillna(0).astype(int)
+    #tb_client["e_top_age_ok"] = np.where(tb_client[age_declare].notna(), 1, 2)
+    tb_client['e_top_age_ok'] = np.where((age_declare!="NA")&(age_declare in tb_client.columns),
+                                            np.where(tb_client["age_declare"].notna(), 1,2),
+                                            np.where(tb_client['e_age'].isna(), 3, 2))
 
-    enriched_data['e_reg'] = enriched_data['e_reg'].round().astype(int)
 
-    column_names = ['region_9','Dept']  # Remplacez par vos noms de colonnes
-    reg9 = pd.read_csv(config['paths']['departements'], sep=',', names=column_names, header=None,on_bad_lines='skip')
-    # Fusion des informations régionales à partir du reg
-    enriched_data = enriched_data.merge(
-        reg9,
-        left_on='e_dept',
-        right_on='Dept',  
-        how='left'
-    )
-    
-    # Renommage des colonnes pour correspondre aux champs de sortie
-    enriched_data.rename(columns={  
-        'region_9': 'e_region_9',        
-    }, inplace=True)
 
-    
-    column_names = ['CODGEO','Seg1','sous_seg1']  # Remplacez par vos noms de colonnes
-    commerces = pd.read_csv(config['paths']['commerces'], sep=',', names=column_names)
-    # Fusion des informations régionales à partir du reg
-    enriched_data = enriched_data.merge(
-        commerces,
-        left_on='codgeo',
-        right_on='CODGEO',  
-        how='left'
-    )
-    enriched_data.drop(columns="CODGEO", inplace=True)
-    # Renommage des colonnes pour correspondre aux champs de sortie
-    enriched_data.rename(columns={  
-        'Seg1': 'e_seg_commerces', 
-        'sous_seg1': 'e_sous_commerces'
 
-    }, inplace=True)
+    return tb_client
 
-    
-    return enriched_data
-'''
-# Exemple d'utilisation
-table_initiale = pd.DataFrame({
-    "codgeo": ["64012", "75101", "010010000"],
-    "sexe": ["M", "F", "M"],
-    "age": [30, 25, 40]
+'''# Exemple d'utilisation
+tb_client = pd.DataFrame({
+    "prenom": ["adina", "Bob", "Clara", "David", "Julian", "Karine"],
+    "codgeo": ["123450000", "234560000", "345670000", "456780000", "567890000", "678910000"],
+    "age_declare": [25, None, 45, None, 32, None],
+    "sexe": ["F", "M", "NA", "M", "M", "F"]
 })
 
-# Appel de la fonction en spécifiant le chemin du fichier CSV de probabilités
-testGeomk = EG_Enrichissement_Geomk(table_initiale, "codgeo", "sexe", "age")
+result = EG_age_sexe(
+    tb_client=tb_client,
+    prenom="prenom",
+    sexe="NA",
+    age_declare="age_declare",
+    top_estim_sexe=1,       # Estimation du sexe à partir du prénom
+    codgeo="codgeo",
+    ajust=1,                # Ajustement de l'âge activé
+    var_ajust="sexe"        # Ajustement basé sur la variable "sexe"
+)
 
 # Affichage du résultat
-print(testGeomk)'''
+print(result)'''
