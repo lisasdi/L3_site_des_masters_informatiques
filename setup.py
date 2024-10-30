e433f45d0acd91ad40614b0f5e2978c12a2d3e6a
lisasdi <79017844+lisasdi@users.noreply.github.com>
2024-10-30 19:19:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-[paths]
-# chemins pour les fichiers
-datainsee = data/correspondance-code-insee-code-postal.csv
-annee=2024
-prenoms=data/Prenoms.csv
-
-
-
-enfants=data/enfants_enfants.csv
-departements=data/DEPT_REGION_UDA.csv
-commerces=data/typo_commerces_typo_commerces.csv
-majdata=data/MAJ_2014_REFERENCES_MAJ_2014_REFERENCES.csv
-
-# fichier entree
-table_init=data/file_init
-#sortie 
-Profil_cible_totale=chemin/Profil_cible_totale
-Profil_cible1_cible2=chemin/Profil_cible1_cible2
-Profil_zone_geo=chemin/Profil_zone_geo
+pip install -r requirements.txt
+main : c'est le fichier _init_.py
+il faut spécifier le chemin du fichier à tester dans config.ini dans la variable : table_init 
+les résultats sont dans chemin/profil
